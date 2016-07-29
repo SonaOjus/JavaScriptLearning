fb0b7a20224e0951e7d0e9cca42bda1edc04d1bd
SonaOjus <sona.ojus@razorthink.net>
2016-07-29 14:44:27 +0530

    Used Arrays of objects

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 
 
-           
+var taskArray = new Array();           
+
 document.getElementById('newTask').onkeypress = function(e)
   {
+    // detect the Enter key Press as Enter key code is "13"
     if (!e) e = window.event;
        var keyCode = e.keyCode || e.which;
         if (keyCode == '13')
         {
-           
+            // created an object to store an ID and name of the task
+            var taskObj = new Object();
+            taskObj.ID = document.getElementById("myTable").rows.length;
+            taskObj.task = document.getElementById("newTask").value
+
+            // store the task objects in the arrays
+            taskArray.push(taskObj);
+
+           // initialise the table and its cells
             var table = document.getElementById("myTable");
             var row   = table.insertRow(document.getElementById("myTable").rows.length);
             var cell1 = row.insertCell(0);
@@ -15,36 +25,62 @@ document.getElementById('newTask').onkeypress = function(e)
             var cell3 = row.insertCell(2);
             
           
-          var checkbox = document.createElement('input'); 
+           // cell[0] is having check box
+            var checkbox = document.createElement('input'); 
             checkbox.type = 'checkbox';
             checkbox.id = "checkbox";
             checkbox.onclick =(function()
                                  {
-                                  var targetElem = this.parentNode.parentNode.cells[1].innerHTML;
-                                  cell2.innerHTML = '<del>' + targetElem + '</del>';
+                                  if (checkbox.checked == true) 
+                                      {
+                                      var rowIndice = this.parentNode.parentNode.rowIndex;
+                                      var targetElem = taskArray[rowIndice].task;
+                                      cell2.innerHTML = '<del>' + targetElem + '</del>';
+                                      }
+                                  else
+                                      {
+                                      var rowIndice = this.parentNode.parentNode.rowIndex;
+                                      var targetElem = taskArray[rowIndice].task;
+                                      cell2.innerHTML = targetElem;
+                                      } 
                                  }
                                  );
             cell1.appendChild(checkbox);
+            displayTasks();
             
-           cell2.innerHTML = document.getElementById("newTask").value;
-            cell2.style.width = "400px";
-            cell2.style.textAlign = "center";
-            
-          var deleteButton = document.createElement('input'); 
+
+            // cell[2] has the delete button
+           var deleteButton = document.createElement('input'); 
             deleteButton.type = 'button';
             deleteButton.id = "deleteButton";
             deleteButton.value = "delete";
             deleteButton.onclick =(function()
                                   {
-                                    var i = this.parentNode.parentNode.rowIndex;
-                                     document.getElementById("myTable").deleteRow(i);
+                                     var rowInd = this.parentNode.parentNode.rowIndex;
+                                     taskArray.splice(rowInd,1);
+                                     displayTasks();
+                                     table.deleteRow(table.rows.length - 1);
                                    }
                                  );
             cell3.appendChild(deleteButton);
             cell3.style.width = "800px";
             cell3.style.textAlign = "center";
+
+            // clear the add task text box after saving a task
             document.getElementById('newTask').value = '';
             
+       }
+
+       function displayTasks()
+       {
+        // cell[1] has the task name
+            for (var i = 0, row; row = table.rows[i], i < taskArray.length; i++)
+            {
+              row.cells[1].innerHTML = taskArray[i].task;
+              row.cells[1].style.width = "400px";
+              row.cells[1].style.textAlign = "center";
+
+            }
        }
     }
   
